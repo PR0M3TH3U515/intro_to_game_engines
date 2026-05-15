662118719b7304632af4db2f0d0e6fe2392f0721
PR0M3TH3U515 <33643030+PR0M3TH3U515@users.noreply.github.com>
2026-05-14 19:43:27 -0700

    Tweaks

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,19 +3,31 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
+const Dash_multi = 5.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var dash_timer: Timer = $DashTimer
+
+var double_jump: bool = false
+
+var can_dash
 
 func _physics_process(delta):
+	if is_on_floor() and double_jump == true:
+		double_jump = false
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			jump()
+		elif double_jump == false:
+			double_jump =true
+			jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -39,7 +51,19 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func Input.is_action_just_pressed("dash") and can_dash
 
 
 func _on_coin_3_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
+
+func jump(multi: float = 3):
+	velocity.y = JUMP_VELOCITY * multi
+	
+func dash():
+	velocity *= Dash_multi
+	can_dash = false
+	dash_timer.start()
+
+func on_dash_timer
