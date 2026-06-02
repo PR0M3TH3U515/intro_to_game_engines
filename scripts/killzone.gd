394cd665818940265fce61ccb3b2787cbbfd49b1
PR0M3TH3U515 <33643030+PR0M3TH3U515@users.noreply.github.com>
2026-06-02 03:47:59 -0700

    Rolled Back
    Downloaded a prior version and tweak that project and replaced the files and such. Tweaked the camera and window display setting. Fixed some issues with the kill zone, where the parent and collision node weren't together. Also changed some timer settings.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -6,7 +6,7 @@ extends Area2D
 func _on_body_entered(body):
 	print("YOU DIED!?")
 	audio_stream_player_2d.play()
-	Engine.time_scale = 0.5
+	Engine.time_scale = 0.25
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
